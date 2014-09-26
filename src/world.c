34e197d79d64bb1e3166eacb9882251a430ea957
Harvey Hunt <harveyhuntnexus@gmail.com>
2014-09-27 07:01:52 +1000

    Add world gen changes from HarveyHunt

--- src/world.c
+++ src/world.c
@@ -5,9 +5,15 @@
 
 void create_world(int p, int q, world_func func, void *arg) {
     int pad = 1;
+
+    // block ids
     unsigned int grass_block_id = get_item_by_name("grass")->id;
     unsigned int sand_block_id = get_item_by_name("sand")->id;
+    unsigned int cobble_block_id = get_item_by_name("cobble")->id;
+    unsigned int sandstone_block_id = cobble_block_id; // til we create a real sandstone block
     unsigned int first_flower_block_id = get_item_by_name("yellow flower")->id;
+
+    // chunk creation loop
     for (int dx = -pad; dx < CHUNK_SIZE + pad; dx++) {
         for (int dz = -pad; dz < CHUNK_SIZE + pad; dz++) {
             int flag = 1;
@@ -16,20 +22,30 @@ void create_world(int p, int q, world_func func, void *arg) {
             }
             int x = p * CHUNK_SIZE + dx;
             int z = q * CHUNK_SIZE + dz;
-            float f = simplex2(x * 0.01, z * 0.01, 4, 0.5, 2);
-            float g = simplex2(-x * 0.01, -z * 0.01, 2, 0.9, 2);
+            float f = simplex2(x * 0.01, z * 0.01, 4, 0.2, 2);
+            float g = simplex2(-x * 0.01, -z * 0.01, 2, 0.3, 2);
             int mh = g * 32 + 16;
             int h = f * mh;
             int w = grass_block_id;
-            int t = 12;
+            unsigned int underblock_id = cobble_block_id;
+            int underblock_level = 3;
+            int t = 4;
             if (h <= t) {
                 h = t;
                 w = sand_block_id;
+                underblock_id = sandstone_block_id;
+                underblock_level = 1;
+            }
+
+            // base layer of stone
+            for (int y = 0; y < underblock_level; y++) {
+                func(x, y, z, cobble_block_id, arg);
             }
             // sand and grass terrain
-            for (int y = 0; y < h; y++) {
+            for (int y = underblock_level; y < h; y++) {
                 func(x, y, z, w * flag, arg);
             }
+
             if (w == grass_block_id) {
                 if (SHOW_PLANTS) {
                     // grass
@@ -55,7 +71,7 @@ void create_world(int p, int q, world_func func, void *arg) {
                             for (int oz = -3; oz <= 3; oz++) {
                                 int d = (ox * ox) + (oz * oz) +
                                     (y - (h + 4)) * (y - (h + 4));
-                                if (d < 11) {
+                                if (d < 11 && simplex3(x + ox, y, z + oz, 4, 0.3, 2) > 0.4) {
                                     func(x + ox, y, z + oz, 15, arg);
                                 }
                             }
